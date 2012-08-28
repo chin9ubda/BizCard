f019e5e786eda98eae0fb3d93891083ace0ef424
chin9ubda <chin9ubda@gmail.com>
2012-08-28 17:23:22 +0900

    Pull test

--- BizCard/FirstTabViewController.m
+++ BizCard/FirstTabViewController.m
@@ -11,6 +11,7 @@
 #import "BcTableCell.h"
 #import "DataStruct.h"
 #import "MsgLoadViewController.h"
+#import <AddressBook/AddressBook.h>
 
 #define CardEdit 1
 #define MemberEdit 2
@@ -197,6 +198,73 @@
 }
 
 
+
+-(void)addrLoad{
+    //===================================================//
+    // 주소록의 모든 정보를 구조체에 저장을 합니다.
+    //===================================================//
+    ABAddressBookRef addressBook = ABAddressBookCreate();
+    CFArrayRef allPeople = ABAddressBookCopyArrayOfAllPeople(addressBook);
+    CFIndex nPeople = ABAddressBookGetPersonCount(addressBook);
+    
+    
+    //===================================================//
+    // 저장된 구조체를 돌면서 해당 데이터를 추출해 옵니다.
+    //===================================================//
+    for (int i = 0; i < nPeople ; i++) {
+        ABRecordRef ref = CFArrayGetValueAtIndex(allPeople, i);
+        CFStringRef firstName = ABRecordCopyValue(ref, kABPersonFirstNameProperty);
+        CFStringRef lastName = ABRecordCopyValue(ref, kABPersonLastNameProperty);
+        NSNumber *recordId = [NSNumber numberWithInteger: ABRecordGetRecordID(ref)];
+        
+        NSLog(@"Name : %d-%@ %@", recordId, (firstName != nil) ? (NSString *)CFBridgingRelease(firstName) : @"",
+              (lastName != nil) ? (NSString *)CFBridgingRelease(lastName) : @"");
+        
+        if (firstName != nil)
+            CFRelease(firstName);
+        if (lastName != nil)
+            CFRelease(lastName);
+        
+        // 사진이미지는 여기에 넘어옵니다.
+        if (ABPersonHasImageData(ref)) {
+            // UIImage* image = [UIImage imageWithData:
+            //        (NSData *)ABPersonCopyImageData(ref)];
+            // image를 저장하는 펑션은 여기에 작성하시면 됩니다.
+        }
+        
+        //========================================================//
+        // 전화번호 구조체 및 카테고리 저장/추출
+        // 전화번호 구조체에는 전화번호와 집전화, 핸드폰 이런 카테고리 구분이 있으며
+        // 이것은 Label로 구별합니다.
+        // Label : kABHomeLabel, kABPersonPhoneIPhoneLabel 등등...
+        //=======================================================//
+        ABMultiValueRef phoneNums =
+        (ABMultiValueRef)ABRecordCopyValue(ref, kABPersonPhoneProperty);
+        for (CFIndex j = 0; j < ABMultiValueGetCount(phoneNums); j++) {
+            CFStringRef label = ABMultiValueCopyLabelAtIndex(phoneNums, j);
+            CFStringRef tempRef = (CFStringRef)ABMultiValueCopyValueAtIndex(phoneNums, j);
+            
+            // 전화번호의 형태라벨별로 추출. 다른 형식도 이렇게 추출이 됩니다.
+            if (CFStringCompare(label, kABPersonPhoneMobileLabel, 0) ==
+                kCFCompareEqualTo) {
+                if (tempRef != nil)
+                    NSLog(@"Mobile: %@-%d", (NSString *)tempRef,i);
+            } else if (CFStringCompare(label, kABPersonPhoneIPhoneLabel, 0) ==
+                       kCFCompareEqualTo) {
+                if (tempRef != nil)
+                    NSLog(@"iPhone: %@-%d", (NSString *)tempRef,i);
+            } else if (CFStringCompare(label, kABHomeLabel, 0) ==
+                       kCFCompareEqualTo) {
+                if (tempRef != nil)
+                    NSLog(@"Home:  %@-%d", (NSString *)tempRef,i);
+            }
+            
+            CFRelease(label);
+            CFRelease(tempRef);
+        }
+    }
+    CFRelease(allPeople);
+}
 
 
 // ------------------------------ Group Set ------------------------------ //
